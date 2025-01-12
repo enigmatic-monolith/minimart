4d4a86327d86a88f60d518865eb5aa4a6aa7b01f
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2025-01-12 21:52:24 +0800

    Implement endpoints for task crud operations

--- backend/src/middleware/authorization.ts
+++ backend/src/middleware/authorization.ts
@@ -1,7 +1,10 @@
 import { Response, NextFunction } from "express";
 import { AuthRequest } from "./authentication";
+import { Enums } from "../database.types";
 
-export const authorizeRole = (roles: string[]) => {
+export type AppRole = Enums<'app_role'>;
+
+export const authorizeRole = (roles: AppRole[]) => {
   return (req: AuthRequest, res: Response, next: NextFunction) => {
     if (!req.user || !roles.includes(req.user.user_role)) {
       res.status(403).json({ message: 'Access denied' });
