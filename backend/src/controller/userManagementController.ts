44caa9c325777d401162ce796724fb681ab9ec9d
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2025-01-14 18:10:47 +0800

    F-A1: Ban users

--- backend/src/controller/userManagementController.ts
+++ backend/src/controller/userManagementController.ts
@@ -1,12 +1,11 @@
 import { Response } from "express";
-import { Tables } from "../database.types";
 import { AuthRequest } from "../middleware/authentication";
-import { supabaseAuthClient } from "../services/supabaseClient";
+import { supabaseAuthClient, supabaseClient } from "../services/supabaseClient";
 
 export const getAllUsers = async (req: AuthRequest, res: Response) => {
     const supabase = supabaseAuthClient();
 
-    const { data, error } = await supabase.auth.admin.listUsers();
+    const { data: { users }, error } = await supabase.auth.admin.listUsers();
 
 
     if (error) {
@@ -14,7 +13,64 @@ export const getAllUsers = async (req: AuthRequest, res: Response) => {
         return;
     }
 
-    res.json(data);
+    res.json(users);
 
     return;
+};
+
+export const banUser = async (req: AuthRequest, res: Response) => {
+    const { id } = req.body;
+    const supabase = supabaseClient(req.accessToken ?? '');
+
+    if (typeof id !== 'string') {
+        res.status(400).json({ error: 'Invalid user ID' });
+        return;
+    }
+
+    const { data: bannedUser, error: bannedUserError } = await supabase
+        .from('banned_users')
+        .select('*')
+        .eq('user_id', id);
+    if (bannedUserError) {
+        res.status(500).json({ error: bannedUserError.message });
+        return;
+    }
+
+    if (bannedUser.length > 0) {
+        res.status(400).json({ error: 'User is already banned' });
+        return;
+    }
+
+    const { data, error } = await supabase
+        .from('banned_users')
+        .insert([{ user_id: id }]);
+    
+    if (error) {
+        res.status(500).json({ error: error.message });
+        return;
+    }
+
+    res.status(201).json(data);
+};
+
+export const unbanUser = async (req: AuthRequest, res: Response) => {
+    const { id } = req.body;
+    const supabase = supabaseClient(req.accessToken ?? '');
+
+    if (typeof id !== 'string') {
+        res.status(400).json({ error: 'Invalid user ID' });
+        return;
+    }
+
+    const { data, error } = await supabase
+        .from('banned_users')
+        .delete()
+        .eq('user_id', id);
+    
+    if (error) {
+        res.status(500).json({ error: error.message });
+        return;
+    }
+
+    res.json(data);
 };
--- backend/src/routes/userManagementRoutes.ts
+++ backend/src/routes/userManagementRoutes.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import { getAllUsers } from '../controller/userManagementController';
+import { banUser, getAllUsers, unbanUser } from '../controller/userManagementController';
 import { authorizeRole } from '../middleware/authorization';
 
 const router = express.Router();
 
 router.get("/", authorizeRole(['admin']), getAllUsers);
+
+router.post("/ban", authorizeRole(['admin']), banUser);
+
+router.put("/ban", authorizeRole(['admin']), unbanUser);
 
 export default router;
