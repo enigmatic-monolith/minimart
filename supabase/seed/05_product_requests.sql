f5cbc4d6ff844e7e53fb8a80fd664418e959affd
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2025-01-15 18:11:08 +0800

    Create product request table

---
+++ supabase/seed/05_product_requests.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.product_requests ("name", "desc", requested_by)
+VALUES 
+  ('Almond Milk', 'Request to stock unsweetened almond milk.', '5ae1fa78-ae05-4817-a87c-c1d85cbb00d5'),
+  ('Vegan Snacks', 'Looking for more vegan-friendly snacks.', '5ae1fa78-ae05-4817-a87c-c1d85cbb00d5'),
+  ('Gluten-Free Bread', 'Request for gluten-free bread options.', 'd247f785-882d-4cef-baff-5457e9a6a359'),
+  ('Energy Drinks', 'Please add more sugar-free energy drinks.', 'd247f785-882d-4cef-baff-5457e9a6a359');
