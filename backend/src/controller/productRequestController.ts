f3e4a221fbd566eaa706e15c55911d44ea35fdd9
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2025-01-15 18:11:46 +0800

    Implement create and update endpoint for req

---
+++ backend/src/controller/productRequestController.ts
@@ -0,0 +1,41 @@
+import { Response } from "express";
+import { AuthRequest } from "../middleware/authentication";
+import { supabaseClient } from "../services/supabaseClient";
+import { Enums, Tables } from "../database.types";
+
+export type ProductRequest = Tables<"product_requests">;
+const productRequestsTbl = "product_requests";
+
+export const getAllProductRequests = async (req: AuthRequest, res: Response) => {
+  const supabase = supabaseClient(req.accessToken ?? "");
+
+  const { data, error } = await supabase.from(productRequestsTbl).select("*");
+
+  if (error) {
+    res.status(500).json({ error: error.message });
+    return;
+  }
+
+  res.json(data);
+
+  return;
+};
+
+export const updateProductRequestStatus =
+  (status: Enums<"request_status">) => async (req: AuthRequest, res: Response) => {
+    const { id } = req.params;
+
+    const supabase = supabaseClient(req.accessToken ?? "");
+
+    const { data, error } = await supabase
+      .from(productRequestsTbl)
+      .update({ status: status, reviewed_at: new Date().toISOString(), reviewed_by: req.user.sub })
+      .eq("id", id)
+
+    if (error) {
+      res.status(500).json({ error: error.message });
+      return;
+    }
+
+    res.status(200).json(data);
+  };
